cfb13eeb9fdef1967e8ae1484728d1aa5797c7d0
tony-console-access-at-153964124765 <anthony.wright.dev@gmail.com>
2023-10-24 09:44:35 +1100

    End of section 12

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -13,8 +13,17 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters.ValidateAudience = false;
         options.TokenValidationParameters.NameClaimType = "username";
     });
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("customPolicy", b =>
+    {
+        b.AllowAnyHeader()
+            .AllowAnyMethod().AllowCredentials().WithOrigins(builder.Configuration["ClientApp"]);
+    });
+});
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapReverseProxy();
 
