ec8656a81110bd716612d3306c20eef63e3033a9
tony-console-access-at-153964124765 <anthony.wright.dev@gmail.com>
2023-10-26 17:48:59 +1100

    Added ip address to enable diagnostics page

--- src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
+++ src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
@@ -13,18 +13,13 @@ public class Index : PageModel
 
     public async Task<IActionResult> OnGet()
     {
-        // var localAddresses = new string[] {
-        //     "::ffff:10.49.0.5", "10.49.0.5",
-        //     "::ffff:10.126.0.2", "10.126.0.2",
-        //     "::ffff:172.17.0.1", "172.17.0.1",
-        //     "::ffff:172.18.0.1", "172.18.0.1",
-        //     "::ffff:170.64.141.170", "170.64.141.170",
-        //     "::ffff:10.5.0.6",
-        //     "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
-        // if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
-        // {
-        //     return NotFound();
-        // }
+        var localAddresses = new string[] {
+            "::ffff:172.18.0.2",
+            "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
+        if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
+        {
+            return NotFound();
+        }
 
         View = new NewDiagnosticsPageViewModel(await HttpContext.AuthenticateAsync(), HttpContext.Connection.RemoteIpAddress.ToString());
 
